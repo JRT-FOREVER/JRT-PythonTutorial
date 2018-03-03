fe51f3bfbc47d3e029908b0ba94145d48e9a67a8
caoxm-me <caoxm1114@outlook.com>
2018-03-03 12:29:08 +0800

    update print.py

--- 01/print.py
+++ 01/print.py
@@ -7,4 +7,9 @@ print("""Hello,JRT!
          Hello,World!""")
 print('''Hello,JRT!
          Hello,World!''')
+# 不仅仅可以接受字符串来输出（最终会转化为字符串类型）
+print("1 + 1 =", 1+1)
+print("2 * 5 =", 2*5)
+print("9 / 3 =", 9/3)
+print("1 - 1 =", 1-1)
 
