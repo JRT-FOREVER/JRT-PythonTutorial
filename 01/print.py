866a3a55e62b0b2a00d1b78d5a519c5d2b5e01ef
caoxm-me <caoxm1114@outlook.com>
2018-03-03 12:25:18 +0800

    print.py

---
+++ 01/print.py
@@ -0,0 +1,10 @@
+# print Hello,JRT!
+print("Hello,JRT!")
+# print Hello,JRT!,Hello,World!--->print函数接受可变数量的参数
+print("Hello,JRT!", "Hello,World!")
+# triple quoted strings,三引号括起来的带有格式的字符串
+print("""Hello,JRT!
+         Hello,World!""")
+print('''Hello,JRT!
+         Hello,World!''')
+
