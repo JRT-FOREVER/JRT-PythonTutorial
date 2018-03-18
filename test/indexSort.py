521754bd3064f20502810a6d97c4c479f32ebd8c
caoxm-me <caoxm1114@outlook.com>
2018-03-18 21:10:23 +0800

    none

--- test/indexSort.py
+++ test/indexSort.py
@@ -1,12 +1,24 @@
-def index_sort(seq):
+def index_sort1(seq):
     seq = enumerate(seq)  # 返回列表的索引-值对
     seq_index = []
     seq_value = []
     for i, j in seq:
         seq_index.append(i)
         seq_value.append(j)
-    return sorted(seq_index, key=lambda x:seq_value[x])
+    return sorted(seq_index, key=lambda x: seq_value[x])
 
 
+
+def index_sort2(seq):
+    seq = enumerate(seq)
+    seq_sorted = sorted(seq, key=lambda x: x[1])
+    dict_sorted = dict(seq_sorted)
+    dict_key = dict_sorted.keys()
+    dict_value = dict_sorted.values()
+    return dict_key, dict_value
 if __name__ == '__main__':
-    print(index_sort((1, -2, 3, 99, 0, 22)))
+    print(index_sort1((1, -2, 3, 99, 0, 22)))
+    # print(index_sort2((1, -2, 3, 99, 0, 22)))
+    a, b = index_sort2((1, -2, 3, 99, 0, 22))
+    print(a)
+    print(b)
